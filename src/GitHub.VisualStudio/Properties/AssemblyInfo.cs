98a24f9787355895a40b7f370c946e3beb9727a1
Jamie Cansdale <jcansdale@gmail.com>
2017-03-20 12:18:22 +0000

    Use ProvideBindingRedirection instead of ProvideCodeBase

--- src/GitHub.VisualStudio/Properties/AssemblyInfo.cs
+++ src/GitHub.VisualStudio/Properties/AssemblyInfo.cs
@@ -6,13 +6,21 @@ using Microsoft.VisualStudio.Shell;
 [assembly: AssemblyDescription("GitHub for Visual Studio VSPackage")]
 [assembly: Guid("fad77eaa-3fe1-4c4b-88dc-3753b6263cd7")]
 
-[assembly: ProvideCodeBase(AssemblyName = "GitHub.UI", CodeBase = @"$PackageFolder$\GitHub.UI.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "GitHub.VisualStudio.UI", CodeBase = @"$PackageFolder$\GitHub.VisualStudio.UI.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "GitHub.Exports", CodeBase = @"$PackageFolder$\GitHub.Exports.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "GitHub.Extensions", CodeBase = @"$PackageFolder$\GitHub.Extensions.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "Octokit", CodeBase = @"$PackageFolder$\Octokit.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "LibGit2Sharp", CodeBase = @"$PackageFolder$\LibGit2Sharp.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "Splat", CodeBase = @"$PackageFolder$\Splat.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "Rothko", CodeBase = @"$PackageFolder$\Rothko.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "GitHub.VisualStudio.TestAutomation.AutomationIDs",
-    CodeBase = @"$PackageFolder$\GitHub.VisualStudio.TestAutomation.AutomationIDs.dll")]
+[assembly: ProvideBindingRedirection(AssemblyName = "GitHub.UI", CodeBase = @"$PackageFolder$\GitHub.UI.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "GitHub.VisualStudio.UI", CodeBase = @"$PackageFolder$\GitHub.VisualStudio.UI.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "GitHub.Exports", CodeBase = @"$PackageFolder$\GitHub.Exports.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "GitHub.Extensions", CodeBase = @"$PackageFolder$\GitHub.Extensions.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "Octokit", CodeBase = @"$PackageFolder$\Octokit.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "LibGit2Sharp", CodeBase = @"$PackageFolder$\LibGit2Sharp.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "Splat", CodeBase = @"$PackageFolder$\Splat.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "Rothko", CodeBase = @"$PackageFolder$\Rothko.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
+[assembly: ProvideBindingRedirection(AssemblyName = "GitHub.VisualStudio.TestAutomation.AutomationIDs", CodeBase = @"$PackageFolder$\GitHub.VisualStudio.TestAutomation.AutomationIDs.dll",
+    OldVersionLowerBound = "LowestMajor", OldVersionUpperBound = "Current")]
